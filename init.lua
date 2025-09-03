646afcfa1c27f17f843d57ecf1d1f65a269c5d4c
mpaq <mathieu.m.paquette@gmail.com>
2025-09-03 13:34:57 -0400

    rust support

--- init.lua
+++ init.lua
@@ -631,6 +631,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        -- codelldb = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -647,6 +648,14 @@ require('lazy').setup({
         html = {
           filetypes = {
             'html',
+          },
+        },
+
+        cssls = {
+          init_options = {
+            provideFormatter = true,
+          },
+          filetypes = {
             'css',
           },
         },
